2103c18fac5c2ccd545b59f1a1f57d40bce48b25
Yanke Guo <guoyk.cn@gmail.com>
2018-08-26 09:05:47 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ build_grpc () {
 
 build_binfs () {
     cd ${SRC_ROOT}/web/ui && yarn build
-    cd ${SRC_ROOT}/web && PKG=web binfs public > public.bfs.go
+    cd ${SRC_ROOT}/web && PKG=web ${GOPATH}/bin/binfs public > public.bfs.go
 }
 
 build_cmd () {
